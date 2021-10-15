cae1a75ae6de0b1d4f6b5aa4e29d263a8bb94951
artemij <Lunkov_A@utkonos.ru>
2021-10-15 20:28:24 +0300

    init

---
+++ android-sdk/src/main/java/ru/apps_point/sdk/ImageValue.kt
@@ -0,0 +1,24 @@
+package ru.apps_point.sdk
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+import coil.compose.rememberImagePainter
+
+sealed class ImageValue {
+
+    class Url(val value: String) : ImageValue()
+
+    class Resource(val value: Int) : ImageValue()
+
+    class Vector(val value: ImageVector) : ImageValue()
+
+    @Composable
+    fun toPainter(): Painter = when (this) {
+        is Resource -> painterResource(value)
+        is Url -> rememberImagePainter(value)
+        is Vector -> rememberVectorPainter(value)
+    }
+}
